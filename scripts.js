b447a76634b410ba3669f44bca6b3294d17af54e
Rachel Lewis <RLewis11769@gmail.com>
2021-10-06 19:28:57 +0000

    Add loader - doesn't reload on click

--- scripts.js
+++ scripts.js
@@ -4,10 +4,8 @@ let responsiveCarousel = {
 	slidesToShow: 4,
 	slidesToScroll: 1,
 	arrows: true,
-	prevArrow: `<img class="a-left control-c slick-prev"src="images/arrow_black_left.png" aria-hidden="true" alt="next">
-		<span class="sr-only">Previous</span>`,
-	nextArrow: `<img class="a-left control-c slick-next" src="images/arrow_black_right.png" aria-hidden="true" alt="next">
-		<span class="sr-only">Previous</span>`,
+	prevArrow: `<img class="a-left control-c slick-prev" src="images/arrow_black_left.png" aria-hidden="true" alt="prev">`,
+	nextArrow: `<img class="a-right control-c slick-next" src="images/arrow_black_right.png" aria-hidden="true" alt="next">`,
 	responsive: [
 		{
 			breakpoint: 992,
@@ -37,17 +35,15 @@ let singleCarousel = {
 	autoplay: true,
 	infinite: true,
 	arrows: true,
-	prevArrow: `<img class="a-left control-c slick-prev"src="images/arrow_white_left.png" aria-hidden="true" alt="next">
-		<span class="sr-only">Previous</span>`,
-	nextArrow: `<img class="a-left control-c slick-next" src="images/arrow_white_right.png" aria-hidden="true" alt="next">
-		<span class="sr-only">Previous</span>`
+	prevArrow: `<img class="a-left control-c slick-prev" src="images/arrow_white_left.png" aria-hidden="true" alt="prev">`,
+	nextArrow: `<img class="a-right control-c slick-next" src="images/arrow_white_right.png" aria-hidden="true" alt="next">`
 }
 
 function addQuote(data) {
 	$('.quotes').prepend(`<div>
 		<div class="row align-items-center justify-content-center">
 			<div class="col-md-4 text-center">
-				<img class="rounded-circle m-5 w-50" src="${data['pic_url']}" alt="">
+				<img class="rounded-circle w-50" src="${data['pic_url']}" alt="">
 			</div>
 			<div class="col-md-5">
 				<div class="card-body">
@@ -63,19 +59,29 @@ function addQuote(data) {
 }
 
 function loadTestimonials() {
-	url = 'https://smileschool-api.hbtn.info/quotes';
-	$.get(url, function (data) {
-		data.forEach(addQuote);
-		$('.quotes').slick(singleCarousel);
-	}).fail(function () {
-		console.log('uh oh');
-	})
+	displayLoading(1);
+	$.ajax({
+		type: 'GET',
+		url: 'https://smileschool-api.hbtn.info/quotes',
+		success: ((data)=> (data.forEach(addQuote))),
+		error: (()=> console.log('uh oh')),
+		complete: (()=> $('.quotes').slick(singleCarousel))
+	});
+	displayLoading(0);
+}
+
+function displayLoading(loading) {
+	$('quotes').empty();
+	if (loading) {
+		$('.quotes').wrap('<div class="loader"></div>');
+	} else {
+		$('.quotes').unwrap();
+	}
 }
 
 $(document).ready(function () {
+	loadTestimonials();
+
 	$('#popular').slick(responsiveCarousel);
 	$('#latest').slick(responsiveCarousel);
-
-	loadTestimonials();
-	console.log('done')
 });

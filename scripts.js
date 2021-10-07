3ebc5154b4f09d2e4caa916cfe3805c1a1d0bb38
Rachel Lewis <RLewis11769@gmail.com>
2021-10-07 15:39:12 +0000

    Add data to homepage and update loader - still not working on click

--- scripts.js
+++ scripts.js
@@ -41,7 +41,7 @@ let singleCarousel = {
 
 function loadTestimonials() {
 	// Query for data for testimonials section (single-item carousel found in multiple places)
-	displayLoading(1);
+	displayLoading(1, '.quotes');
 	$.ajax({
 		type: 'GET',
 		url: 'https://smileschool-api.hbtn.info/quotes',
@@ -49,7 +49,7 @@ function loadTestimonials() {
 		error: (()=> console.log('Unable to load data')),
 		complete: (()=> $('.quotes').slick(singleCarousel))
 	});
-	displayLoading(0);
+	displayLoading(0, '.quotes');
 }
 
 function addTestimonial(data) {
@@ -72,33 +72,34 @@ function addTestimonial(data) {
 	</div>`);
 }
 
-function loadPopularTutorials() {
-	// Query for data for popular tutorials video section (multi-item carousel)
-	displayLoading(1);
+function loadTutorials(url, id) {
+	// Query for data for popular or latest tutorials video section (multi-item carousel)
+	displayLoading(1, `.${id}`);
 	$.ajax({
 		type: 'GET',
-		url: 'https://smileschool-api.hbtn.info/popular-tutorials',
-		success: (data)=> {
+		url: url,
+		success: (videos)=> {
 			// for each tutorial, add html data and then add rating stars
-			data.forEach(addTutorial);
-			data.forEach(includeStars);
+			for (let video of videos) {
+				addTutorial(video, id);
+				includeStars(video, id);
+			}
 		},
 		error: (()=> console.log('Unable to load data')),
-		complete: (()=> $('#popular').slick(responsiveCarousel))
+		complete: (()=> $(`#${id}`).slick(responsiveCarousel))
 	});
-	displayLoading(0);
+	displayLoading(0, `.${id}`);
 }
 
-function addTutorial (data) {
-	// Add html to #popular div including dynamic data
-	$('#popular').prepend(`<div class="mx-2" id="popular${data.id}">
+function addTutorial (data, id) {
+	// Add html to #popular or #latest div including dynamic data
+	$(`#${id}`).prepend(`<div class="mx-2" id="${id}${data.id}">
 			<img class="card-img-top" src="${data["thumb_url"]}" alt="">
 			<div class="card-body mx-0">
 				<h1 class="card-title lead font-weight-bold">${data.title}</h1>
 				<p class="card-text text-secondary">${data["sub-title"]}</p>
 				<div class="row">
 					<img class="rounded-circle ml-2" src="${data["author_pic_url"]}" height="35px" width="35px" alt="">
-					<img class="card-img-overlay" src="images/play.png" alt="">
 					<p class="ml-3 mt-1 purple">${data.author}</p>
 				</div>
 				<div class="row align-items-center justify-content-between px-4">
@@ -109,29 +110,30 @@ function addTutorial (data) {
 		</div>`);
 }
 
-function includeStars (data) {
+function includeStars (data, id) {
 	// Add star rating to video tutorials
 	for (let on = 0; on < data.star; on++) {
-		$(`#star${data.id}`).append('<img src="images/star_on.png" height="25px" width="25px" alt="">');
+		$(`#${id} #star${data.id}`).append('<img src="images/star_on.png" height="25px" width="25px" alt="">');
 	}
 	for (let off = 5; off > data.star; off--) {
-		$(`#star${data.id}`).append('<img src="images/star_off.png" height="25px" width="25px" alt="">');
+		$(`#${id} #star${data.id}`).append('<img src="images/star_off.png" height="25px" width="25px" alt="">');
 	}
 }
 
-function displayLoading(loading) {
+function displayLoading(loading, tag) {
 	// Adds spinner to indicate loading while API is working
-	$('.quotes').empty();
+	$(tag).empty();
 	if (loading) {
-		$('.quotes').wrap('<div class="loader"></div>');
+		$(tag).wrap('<div class="loader"></div>');
 	} else {
-		$('.quotes').unwrap();
+		$(tag).unwrap();
 	}
 }
 
 $(document).ready(function () {
+	displayLoading(1, 'body');
 	loadTestimonials();
-	loadPopularTutorials();
-
-	$('#latest').slick(responsiveCarousel);
+	loadTutorials('https://smileschool-api.hbtn.info/popular-tutorials', 'popular');
+	loadTutorials('https://smileschool-api.hbtn.info/latest-videos', 'latest');
+	displayLoading(0, 'body');
 });

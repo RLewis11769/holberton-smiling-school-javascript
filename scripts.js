a1d108789cfa4896bf5c49d7e6ed27e51c23e254
Rachel Lewis <RLewis11769@gmail.com>
2021-10-07 15:18:29 +0000

    Add data to 2 - multi-item carousel

--- scripts.js
+++ scripts.js
@@ -39,13 +39,27 @@ let singleCarousel = {
 	nextArrow: `<img class="a-right control-c slick-next" src="images/arrow_white_right.png" aria-hidden="true" alt="next">`
 }
 
-function addQuote(data) {
+function loadTestimonials() {
+	// Query for data for testimonials section (single-item carousel found in multiple places)
+	displayLoading(1);
+	$.ajax({
+		type: 'GET',
+		url: 'https://smileschool-api.hbtn.info/quotes',
+		success: (data)=> (data.forEach(addTestimonial)),
+		error: (()=> console.log('Unable to load data')),
+		complete: (()=> $('.quotes').slick(singleCarousel))
+	});
+	displayLoading(0);
+}
+
+function addTestimonial(data) {
+	// Add html to .quotes div including dynamic data
 	$('.quotes').prepend(`<div>
 		<div class="row align-items-center justify-content-center">
 			<div class="col-md-4 text-center">
-				<img class="rounded-circle w-50" src="${data['pic_url']}" alt="">
+				<img class="rounded-circle w-50 mx-auto" src="${data['pic_url']}" alt="">
 			</div>
-			<div class="col-md-5">
+			<div class="col-md-6">
 				<div class="card-body">
 					<h1 class="lead">${data.text}</h1>
 					<p>
@@ -58,20 +72,56 @@ function addQuote(data) {
 	</div>`);
 }
 
-function loadTestimonials() {
+function loadPopularTutorials() {
+	// Query for data for popular tutorials video section (multi-item carousel)
 	displayLoading(1);
 	$.ajax({
 		type: 'GET',
-		url: 'https://smileschool-api.hbtn.info/quotes',
-		success: ((data)=> (data.forEach(addQuote))),
-		error: (()=> console.log('uh oh')),
-		complete: (()=> $('.quotes').slick(singleCarousel))
+		url: 'https://smileschool-api.hbtn.info/popular-tutorials',
+		success: (data)=> {
+			// for each tutorial, add html data and then add rating stars
+			data.forEach(addTutorial);
+			data.forEach(includeStars);
+		},
+		error: (()=> console.log('Unable to load data')),
+		complete: (()=> $('#popular').slick(responsiveCarousel))
 	});
 	displayLoading(0);
 }
 
+function addTutorial (data) {
+	// Add html to #popular div including dynamic data
+	$('#popular').prepend(`<div class="mx-2" id="popular${data.id}">
+			<img class="card-img-top" src="${data["thumb_url"]}" alt="">
+			<div class="card-body mx-0">
+				<h1 class="card-title lead font-weight-bold">${data.title}</h1>
+				<p class="card-text text-secondary">${data["sub-title"]}</p>
+				<div class="row">
+					<img class="rounded-circle ml-2" src="${data["author_pic_url"]}" height="35px" width="35px" alt="">
+					<img class="card-img-overlay" src="images/play.png" alt="">
+					<p class="ml-3 mt-1 purple">${data.author}</p>
+				</div>
+				<div class="row align-items-center justify-content-between px-4">
+					<div class="row" id="star${data.id}"></div>
+					<p class="purple ml-2 pt-3">${data.duration}</p>
+				</div>
+			</div>
+		</div>`);
+}
+
+function includeStars (data) {
+	// Add star rating to video tutorials
+	for (let on = 0; on < data.star; on++) {
+		$(`#star${data.id}`).append('<img src="images/star_on.png" height="25px" width="25px" alt="">');
+	}
+	for (let off = 5; off > data.star; off--) {
+		$(`#star${data.id}`).append('<img src="images/star_off.png" height="25px" width="25px" alt="">');
+	}
+}
+
 function displayLoading(loading) {
-	$('quotes').empty();
+	// Adds spinner to indicate loading while API is working
+	$('.quotes').empty();
 	if (loading) {
 		$('.quotes').wrap('<div class="loader"></div>');
 	} else {
@@ -81,7 +131,7 @@ function displayLoading(loading) {
 
 $(document).ready(function () {
 	loadTestimonials();
+	loadPopularTutorials();
 
-	$('#popular').slick(responsiveCarousel);
 	$('#latest').slick(responsiveCarousel);
 });
